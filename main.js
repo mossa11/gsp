87fe7af9caed9f12e0fb86f4ba3ab1b6d4208854
mossa11 <mossyartist@gmail.com>
2026-05-20 11:29:46 +0100

    m

---
+++ main.js
@@ -0,0 +1,10 @@
+let num1 = 28;
+let num2 = 15;
+
+if (num1 > num2) {
+    console.log("num1 is greater");
+} else if (num1 < num2) {
+    console.log("num2 is greater");
+} else if (num1 === num2) {
+    console.log("They are equal");
+}
